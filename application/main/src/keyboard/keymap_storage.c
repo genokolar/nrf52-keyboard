a2b288883a92bb679eac1fb03bc3281e0631da14
Geno [格诺] <genokolar@gmail.com>
2019-08-06 11:50:45 +0800

    修复禁用keymap storage的编译错误

--- application/main/src/keyboard/keymap_storage.c
+++ application/main/src/keyboard/keymap_storage.c
@@ -175,5 +175,7 @@ bool keymap_set(uint8_t block, uint8_t size, uint8_t* data)
 
 #else
 void keymap_init(void) {}
-bool keymap_set(uint8_t block, uint8_t size, uint8_t* data) {}
+bool keymap_set(uint8_t block, uint8_t size, uint8_t* data) {
+	return false;
+}
 #endif
