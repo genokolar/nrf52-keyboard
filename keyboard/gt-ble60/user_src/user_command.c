dca7d2474a364cae7b046a6b1565cc1500ea0b5f
Geno [格诺] <genokolar@gmail.com>
2019-08-18 10:51:25 +0800

    调整User Command 加入清空绑定

--- keyboard/gt-ble60/user_src/user_command.c
+++ keyboard/gt-ble60/user_src/user_command.c
@@ -34,16 +34,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdbool.h>
 #include <stdint.h>
 
-APP_TIMER_DEF(command_run_timer);
+APP_TIMER_DEF(bootloader_run_timer);
+APP_TIMER_DEF(bonds_run_timer);
+APP_TIMER_DEF(devices_run_timer);
+uint8_t devices_id = 0;
 
 /**
  * @brief command运行handler
  * 
  * @param context 
  */
-static void command_timer_handler(void* p_context)
+static void bootloader_handler(void* p_context)
 {
     bootloader_jump();
+}
+
+static void bonds_handler(void* p_context)
+{
+    delete_bonds();
+}
+
+static void devices_handler(void* p_context)
+{
+    restart_advertising_id(devices_id);
 }
 
 /**
@@ -52,7 +65,9 @@ static void command_timer_handler(void* p_context)
  */
 void command_timer_init(void)
 {
-    app_timer_create(&command_run_timer, APP_TIMER_MODE_SINGLE_SHOT, command_timer_handler);
+    app_timer_create(&bootloader_run_timer, APP_TIMER_MODE_SINGLE_SHOT, bootloader_handler);
+    app_timer_create(&bonds_run_timer, APP_TIMER_MODE_SINGLE_SHOT, bonds_handler);
+    app_timer_create(&devices_run_timer, APP_TIMER_MODE_SINGLE_SHOT, devices_handler);
 }
 
 bool command_extra(uint8_t code)
@@ -72,33 +87,43 @@ bool command_extra(uint8_t code)
 #endif
         break;
     case KC_L:
+	case KC_SLSH:
         //显示状态灯
         if (!usb_working()) {
             clear_keyboard();
             status_led_display();
         }
         break;
+    case KC_D:
+        //清空绑定数据
+        clear_keyboard();
+        app_timer_start(bonds_run_timer, APP_TIMER_TICKS(1000), NULL);
+        break;
     //多设备切换：支持4台设备切换
     case KC_Q:
         clear_keyboard();
-        restart_advertising_id(0);
+        devices_id = 0;
+        app_timer_start(devices_run_timer, APP_TIMER_TICKS(200), NULL);
         break;
     case KC_W:
         clear_keyboard();
-        restart_advertising_id(1);
+        devices_id = 1;
+        app_timer_start(devices_run_timer, APP_TIMER_TICKS(200), NULL);
         break;
     case KC_E:
         clear_keyboard();
-        restart_advertising_id(2);
+        devices_id = 2;
+        app_timer_start(devices_run_timer, APP_TIMER_TICKS(200), NULL);
         break;
     case KC_R:
         clear_keyboard();
-        restart_advertising_id(3);
+        devices_id = 3;
+        app_timer_start(devices_run_timer, APP_TIMER_TICKS(1000), NULL);
         break;
     case KC_B:
         //重启到DFU模式
         clear_keyboard();
-        app_timer_start(command_run_timer, APP_TIMER_TICKS(1000), NULL);
+        app_timer_start(bootloader_run_timer, APP_TIMER_TICKS(1000), NULL);
         break;
     default:
         return false;
