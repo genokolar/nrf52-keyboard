605ffd2e2b84740a7bf9baf665c9e7bb1f86cc25
Geno [格诺] <genokolar@gmail.com>
2019-08-25 17:33:48 +0800

    添加所需SDK15.3
    基于SDK下载困难，直接加入SDK

---
+++ SDK/external/micro-ecc/nrf52nf_armgcc/armgcc/_build/micro_ecc_lib/uECC.c.d
@@ -0,0 +1,41 @@
+_build/micro_ecc_lib/uECC.c.o: ../../micro-ecc/uECC.c \
+ ../../micro-ecc/uECC.h \
+ d:\app\gcc\2018q2\lib\gcc\arm-none-eabi\7.3.1\include\stdint.h \
+ d:\app\gcc\2018q2\arm-none-eabi\include\stdint.h \
+ d:\app\gcc\2018q2\arm-none-eabi\include\machine\_default_types.h \
+ d:\app\gcc\2018q2\arm-none-eabi\include\sys\features.h \
+ d:\app\gcc\2018q2\arm-none-eabi\include\_newlib_version.h \
+ d:\app\gcc\2018q2\arm-none-eabi\include\sys\_intsup.h \
+ d:\app\gcc\2018q2\arm-none-eabi\include\sys\_stdint.h \
+ ../../micro-ecc/uECC_vli.h ../../micro-ecc/types.h \
+ ../../micro-ecc/platform-specific.inc ../../micro-ecc/asm_arm.inc \
+ ../../micro-ecc/asm_arm_mult_square_umaal.inc \
+ ../../micro-ecc/curve-specific.inc
+
+../../micro-ecc/uECC.h:
+
+d:\app\gcc\2018q2\lib\gcc\arm-none-eabi\7.3.1\include\stdint.h:
+
+d:\app\gcc\2018q2\arm-none-eabi\include\stdint.h:
+
+d:\app\gcc\2018q2\arm-none-eabi\include\machine\_default_types.h:
+
+d:\app\gcc\2018q2\arm-none-eabi\include\sys\features.h:
+
+d:\app\gcc\2018q2\arm-none-eabi\include\_newlib_version.h:
+
+d:\app\gcc\2018q2\arm-none-eabi\include\sys\_intsup.h:
+
+d:\app\gcc\2018q2\arm-none-eabi\include\sys\_stdint.h:
+
+../../micro-ecc/uECC_vli.h:
+
+../../micro-ecc/types.h:
+
+../../micro-ecc/platform-specific.inc:
+
+../../micro-ecc/asm_arm.inc:
+
+../../micro-ecc/asm_arm_mult_square_umaal.inc:
+
+../../micro-ecc/curve-specific.inc:
