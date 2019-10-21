aad8c6e617d1d832c3fe45cb86470bcbdfc2dba5
Geno [格诺] <genokolar@gmail.com>
2019-10-21 22:24:38 +0800

    [keyboard]将ble60 farad69 的默认按键同步到与在线编译一致

--- keyboard/farad69/keymap_plain.c
+++ keyboard/farad69/keymap_plain.c
@@ -19,21 +19,26 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "user_fn.h"
 
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* 0: default */
-    KEYMAP(
-        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,   EQL, BSPC,   GRV,\
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   BSLS,   P7,    P8,   P9, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,        ENT,    P4,    P5,   P6,  \
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT,      FN4,    P1,    P2,   P3,\
-        LCTL,LGUI,LALT,SPC,           SPC,      FN0, FN1,      FN2,    FN3  , P0,   DEL ),
+	KEYMAP(
+		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NLCK, KC_BSPC, 
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_P7, KC_P8, KC_P9, 
+		KC_FN0, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT, KC_P4, KC_P5, KC_P6, 
+		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_FN1, KC_UP, KC_P1, KC_P2, KC_P3, 
+		KC_LCTL, KC_LGUI, KC_LALT, KC_FN2, KC_SPC, KC_FN3, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0, KC_PDOT),
+
+	KEYMAP(
+		KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_TRNS, 
+		KC_FN4, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SCLN, KC_QUOT, KC_MPRV, KC_MPLY, KC_MNXT, 
+		KC_TRNS, KC_MYCM, KC_CALC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SLSH, KC_TRNS, KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN)
+
 };
 const action_t fn_actions[] = {
-    /* Poker Layout */
-    [0] = ACTION_LAYER_TAP_KEY(2, KC_DELETE),
-    [1] = ACTION_MODS_TAP_KEY(MOD_RALT, KC_LEFT),
-    [2] = ACTION_LAYER_TAP_KEY(3, KC_DOWN),
-    [3] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_RIGHT),
-    [4] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_UP),
-    [5] = ACTION_FUNCTION(BT_BAT),
+    ACTION_MODS_TAP_KEY(MOD_LCTL, KC_CAPS),
+	ACTION_MODS_TAP_KEY(MOD_RSFT, KC_DOT),
+	ACTION_LAYER_TAP(1, KC_SPC),
+	ACTION_MODS_TAP_KEY(MOD_RCTL, KC_DEL),
+	ACTION_FUNCTION(0)
 };
 
