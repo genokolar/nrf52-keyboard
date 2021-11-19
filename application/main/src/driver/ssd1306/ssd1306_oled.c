1a08bf00fd32b8b0900035632efc227433f27a99
AYST <75466116+jc-make@users.noreply.github.com>
2021-11-19 11:26:56 +0800

    Add files via upload

--- application/main/src/driver/ssd1306/ssd1306_oled.c
+++ application/main/src/driver/ssd1306/ssd1306_oled.c
@@ -162,7 +162,7 @@ static void ssd1306_show_all()
 }
 
 /**
- * @brief 清空Buff
+ * #@brief 清空Buff
  * 
  */
 void ssd1306_clr()
@@ -266,7 +266,7 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
         case KBD_STATE_POST_INIT: // 初始化
             ssd1306_twi_init();
             ssd1306_oled_init();
-            ssd1306_clr();
+            //ssd1306_clr();
             ssd1306_inited = true;
             break;
         case KBD_STATE_INITED: // 显示Buff
